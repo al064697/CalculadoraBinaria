4643c81c65f217087e63a8372421123e082511fd
{{al064697}} <{{al064697@uacam.mx}}>
2023-10-04 17:31:54 -0600

    remove static condition on ln 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
-    static Calculadora calculadora = new Calculadora();
-    public static void main(String[] args) {calculadora.EncenderCalculadora(true);}
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.EncenderCalculadora(true);
+    }
 }
