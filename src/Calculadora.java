b2d7c2a37ad2de5e39183e6ade6eb781e4c8af9b
al064697 <al064697@uacam.mx>
2023-10-06 14:33:18 -0600

    A lot of updates on classes

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,15 +6,16 @@ public class Calculadora {
     int n1, n2;
     public void Operacion() {
         operacion = JOptionPane.showInputDialog(null,
-                "Calculadora de binarios" + ENTER + "Ingresa una operación binaria " + "(suma, resta o multiplicación)");
+                "Calculadora de binarios" + ENTER + "Ingresa una operación binaria "
+                        + "(suma, resta o multiplicación)");
         if((operacion.contains("0") && operacion.contains("1"))) {
             if (operacion.contains("+")) { SumaBinarios(); }
             else if(operacion.contains("x")) { MultiplicacionBinaria(); }
             else if(operacion.contains("-")) { RestaBinaria(); }
         } else {
             while ((!(operacion.equals("0")) && (!(operacion.equals("1"))))) {
-                JOptionPane.showMessageDialog(null, "ERROR: valores no binarios"
-                        + ENTER + "intenta de nuevo");
+                JOptionPane.showMessageDialog(null,
+                        "ERROR: valores no binarios" + ENTER + "intenta de nuevo");
                 Operacion();
             }
         }
@@ -37,35 +38,38 @@ public class Calculadora {
         n1 = Integer.parseInt(operacion.substring(0, operacion.indexOf('+')).trim(), 2);
         n2 = Integer.parseInt(operacion.substring(operacion.indexOf('+') + 1).trim(), 2);
         resultado = Integer.toBinaryString(n1  + n2);
-        JOptionPane.showMessageDialog(null, "El resultado de la suma binaria es "
-                + resultado + " base 2");
+        JOptionPane.showMessageDialog(null,
+                "El resultado de la suma binaria es " + resultado + " base 2");
     }
     public void ApagarCalculadora(int opcion) {
         switch(opcion) {
             case 1:
                 JOptionPane.showMessageDialog(null, "Apagando calculadora...");
-                System.exit(0); break;
+                System.exit(0);
+                break;
             case 0: Opciones(); break;
             default:
-                JOptionPane.showMessageDialog(null, "SYSTEM ERROR. " + ENTER + ENTER
-                        + "Try latter"); System.exit(0);
+                JOptionPane.showMessageDialog(null,
+                        "SYSTEM ERROR. " + ENTER + "Try latter"); System.exit(0);
         }
     }
     public void Opciones() {
         int opcion = 0;
         try {
-            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "Iniciar?"
-                    + ENTER + "Si..........1" + ENTER + "No..........0"));
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Iniciar?" + ENTER + "Si..........1" + ENTER + "No..........0"));
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error: valores no aceptados." + ENTER
-                    + "Intenta desde el principio");
-            Opciones(); System.exit(0);
+            JOptionPane.showMessageDialog(null,
+                    "Error: valores no aceptados." + ENTER + "Intenta desde el principio");
+            Opciones();
+            System.exit(0);
         }
         do {
             switch(opcion) {
                 case 1:
                     JOptionPane.showMessageDialog(null, "Iniciando calculadora...");
-                    Operacion(); break;
+                    Operacion();
+                    break;
                 case 0:
                     try {
                         opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
@@ -74,17 +78,21 @@ public class Calculadora {
                     } catch (NumberFormatException e) {
                         JOptionPane.showMessageDialog(null,
                                 "Error: valores no aceptados." + ENTER + "Intenta desde el principio");
-                        Opciones(); System.exit(0);
+                        Opciones();
+                        System.exit(0);
                     }
-                    ApagarCalculadora(opcion); break;
+                    ApagarCalculadora(opcion);
+                    break;
             }
         } while ((opcion == 1) | (opcion == 0));
         JOptionPane.showMessageDialog(null, "SYSTEM ERROR. " + ENTER +"Try latter");
         System.exit(0);
     }
     public void EncenderCalculadora(boolean estado) {
-        if(estado) { JOptionPane.showMessageDialog(null, "Calculadora encendida"); Opciones(); }
+        if(estado) {
+            JOptionPane.showMessageDialog(null, "Calculadora encendida");
+            Opciones();
+        }
         else { System.out.println(" "); }
     }
-    public static void main(String[] args) { Main.main(args); }
 }
